a2251c39f45cd1bc1a2e3f9ef80396720c910d0c
Your Name <xoxa2@mail.ru>
2021-03-14 16:07:04 +0300

    Выполнил 14 задание.

--- HomeWork14/src/com/company/Main.java
+++ HomeWork14/src/com/company/Main.java
@@ -3,6 +3,42 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// Андрей, добрый день. В ближайшее время сделаю это задание.
+
+    }
+
+    public int[] getValuesAfterLastFour(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 4) {
+                return getSliceArray(i + 1, arr);
+            }
+        }
+        throw new RuntimeException();
+    }
+
+    private int[] getSliceArray(int fromIndex, int[] originalArray) {
+        int[] resultArray = new int[originalArray.length - fromIndex];
+
+        for (int i = fromIndex, j = 0; i < originalArray.length; i++, j++) {
+            resultArray[j] = originalArray[i];
+        }
+
+        return resultArray;
+    }
+
+    public boolean findOneorFourNumberInArray(int[] arr) {
+        boolean countOne = false;
+        boolean countFour = false;
+
+        for (int el : arr) {
+            if (el == 4) {
+                countFour = true;
+            } else if (el == 1) {
+                countOne = true;
+            } else {
+                return false;
+            }
+        }
+
+        return countOne && countFour;
     }
 }
---
+++ HomeWork14/src/test/com/MainTest.java
@@ -0,0 +1,65 @@
+package test.com;
+
+import com.company.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    private final Main main = new Main();
+
+    @ParameterizedTest
+    @MethodSource("dataForSuccessGettingValuesAfterLastFour")
+    void getValuesAfterLastFour(int[] originalArray, int[] expectedResultArray) {
+        int[] aclualResultArray = main.getValuesAfterLastFour(originalArray);
+
+        Assertions.assertArrayEquals(expectedResultArray, aclualResultArray);
+    }
+
+    private static Stream<Arguments> dataForSuccessGettingValuesAfterLastFour() {
+        List<Arguments> arguments = new ArrayList<>();
+        arguments.add(Arguments.arguments(new int[]{1, 2, 3, 4, 5}, new int[]{5}));
+        arguments.add(Arguments.arguments(new int[]{1, 2, 3, 4}, new int[]{}));
+        arguments.add(Arguments.arguments(new int[]{4, 1, 2, 4, 3, 4, 5}, new int[]{5}));
+        arguments.add(Arguments.arguments(new int[]{4, 1, 2, 3, 5}, new int[]{1, 2, 3, 5}));
+
+        return arguments.stream();
+    }
+
+    @Test
+    void getValuesAfterLastFourWaitException() {
+        int[] originalArray = {1, 2, 3};
+        Assertions.assertThrows(RuntimeException.class, () -> main.getValuesAfterLastFour(originalArray));
+
+        int[] empty = {};
+        Assertions.assertThrows(RuntimeException.class, () -> main.getValuesAfterLastFour(empty));
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataForFindFourOrOneNumberInArray")
+    void findFourOrOneNumberInArray(int[] array, boolean expectedResult) {
+        boolean actualResult = main.findOneorFourNumberInArray(array);
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    private static Stream<Arguments> dataForFindFourOrOneNumberInArray() {
+        List<Arguments> arguments = new ArrayList<>();
+        arguments.add(Arguments.arguments(new int[]{1, 1, 1, 4, 4, 1, 4, 4}, true));
+        arguments.add(Arguments.arguments(new int[]{1, 1, 1, 1, 1, 1}, false));
+        arguments.add(Arguments.arguments(new int[]{4, 4, 4, 4}, false));
+        arguments.add(Arguments.arguments(new int[]{1, 4, 4, 1, 1, 4, 3}, false));
+
+        return arguments.stream();
+    }
+
+}
